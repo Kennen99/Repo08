a1735f03113bd8c0e396296a7b82dce175c53653
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-20 22:36:14 +0200

    Added a js file to ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+// Write code below this line
+function functionPrintInfo(name,age) {
+    console.log(name,age)
+}
+functionPrintInfo("Finn", 12);
+// Write code above this line
+
+module.exports = functionPrintInfo;
