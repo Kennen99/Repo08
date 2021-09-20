1aaf6caf0ef00d8123c5d993702e263d2987953c
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-20 23:32:30 +0200

    Added a js file to ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function xSix(x){
+    return x*6;
+}
+console.log(xSix(3));
+console.log(xSix(5));
+console.log(xSix(19));
+// Only change code above this line
+
+module.exports = xSix;
