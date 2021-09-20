1417608ad4a4ea8e546c0775d7a9879ee0e8123d
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-21 00:06:54 +0200

    Added a js file to ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function mathTest(x, y){
+    // Only change code above this line
+    if (x < 0 || y < 0){
+        // Only change code below this line
+        return undefined;
+        // Only change code above this line
+    }
+    return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y),2));
+    // Only change code below this line
+}
+testic = mathTest(3,3);
+console.log(testic);
+// Only change code above this line
+module.exports = mathTest;
